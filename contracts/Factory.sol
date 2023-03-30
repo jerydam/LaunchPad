e1e504ffd041594b916da23476d679e8d08c8dae
Jerydam <jerydan148@gmail.com>
2023-03-30 19:41:01 +0100

    hmmmm

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,19 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
-import {LaunchPad} from "./Launchpad.sol";
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.17;
+// import {LaunchPad} from "./Launchpad.sol";
 
-contract cloneMultiSig {
-    LaunchPad[] private _launchpad;
+// contract cloneLaunchpad {
+//     LaunchPad[] private _launchPad;
 
-    event child(address _child);
+//     event child(address _child);
 
-    function createLau(address[] memory _admins)
-        public
-        returns (LaunchPad newLaunchPad)
-    {
-        newLaunchPad = new LaunchPad(_admins, msg.sender);
-        _launchpad.push(newLaunchPad);
+//     function createMultiSig(address[] memory _admins)
+//         public
+//         returns (LaunchPad newLaunchPad)
+//     {
+//         newLaunchPad = new LaunchPad(_admins, msg.sender);
+//         _launchPad.push(newLaunchPad);
 
-        emit child(address(newLaunchPad));
-    }
-}
+//         emit child(address(newLaunchPad));
+//     }
+// }
